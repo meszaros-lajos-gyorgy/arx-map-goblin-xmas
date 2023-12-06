9fd70ef950fcf058972126f1afa98e394ba9ae41
Lajos Meszaros <m_lajos@hotmail.com>
2023-12-06 21:28:11 +0100

    refactor: remove unused imports and previous model instantiations

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-import {
-  ArxMap,
-  DONT_QUADIFY,
-  HudElements,
-  QUADIFY,
-  SHADING_SMOOTH,
-  Settings,
-  Texture,
-  Vector3,
-} from 'arx-level-generator'
+import { ArxMap, HudElements, QUADIFY, SHADING_SMOOTH, Settings, Texture, Vector3 } from 'arx-level-generator'
 import { createPlaneMesh } from 'arx-level-generator/prefabs/mesh'
 import { Speed } from 'arx-level-generator/scripting/properties'
 import { createLight } from 'arx-level-generator/tools'
 import { applyTransformations, circleOfVectors } from 'arx-level-generator/utils'
-import { MathUtils, Vector2 } from 'three'
-
-// import { Hat } from './entities/hat.js'
-// import { Present } from './entities/present.js'
-// import { xmasTreeMesh } from './prefabs/xmasTree.js'
+import { Vector2 } from 'three'
 
 const settings = new Settings()
 
@@ -40,36 +27,6 @@ plane.translateY(map.config.offset.y)
 plane.translateZ(map.config.offset.z)
 applyTransformations(plane)
 map.polygons.addThreeJsMesh(plane, { tryToQuadify: QUADIFY, shading: SHADING_SMOOTH })
-
-// -------------------------
-
-/*
-const present1 = new Present({
-  position: new Vector3(300, -10, 300),
-  variant: 'red',
-})
-
-const present2 = new Present({
-  position: new Vector3(350, -10, 300),
-  variant: 'blue',
-})
-
-const hat = new Hat({
-  position: new Vector3(200, -100, 250),
-})
-
-map.entities.push(present1, present2, hat)
-
-// ----------------------
-
-xmasTreeMesh[0].geometry.rotateY(MathUtils.degToRad(117))
-applyTransformations(xmasTreeMesh[0])
-xmasTreeMesh[0].geometry.translate(map.config.offset.x, map.config.offset.y - 200, map.config.offset.z + 200)
-
-map.polygons.addThreeJsMesh(xmasTreeMesh[0], { shading: SHADING_SMOOTH, tryToQuadify: DONT_QUADIFY })
-
-map.polygons.selectByTextures(['pngwing.com.png']).flipUVVertically()
-*/
 
 // ----------------------
 
