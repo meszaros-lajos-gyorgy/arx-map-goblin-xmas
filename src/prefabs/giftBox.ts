f67063a782272ef71e0e6fbcc8aed703dcc8fcad
Lajos Meszaros <m_lajos@hotmail.com>
2023-12-10 02:04:14 +0100

    refactor(prefabs/giftBox): utilize Prefab class to shrink code

--- src/prefabs/giftBox.ts
+++ src/prefabs/giftBox.ts
@@ -1,29 +1,9 @@
-import { Rotation, Vector3 } from 'arx-level-generator'
-import { loadOBJ } from 'arx-level-generator/tools/mesh'
+import { Prefab } from '@/Prefab.js'
 
-type createGiftBoxProps = {
-  position?: Vector3
-  orientation?: Rotation
-  scale?: number
-}
+const giftBoxLoader = new Prefab({
+  filenameWithoutExtension: 'Giftbox',
+  yAxisAdjustment: -130,
+  internalScale: 0.25,
+})
 
-export const createGiftBox = async ({
-  position = new Vector3(0, 0, 0),
-  orientation,
-  scale: rawScale = 1,
-}: createGiftBoxProps = {}) => {
-  const scale = 0.1 * rawScale
-
-  return loadOBJ('./3d models + textures/Giftbox', {
-    position: position.clone().add(new Vector3(0, -130 * scale, 0)),
-    scale,
-    orientation,
-    materialFlags: (texture, flags) => {
-      if (!texture.isInternalAsset) {
-        texture.sourcePath = './3d models + textures/Texture/'
-      }
-
-      return flags
-    },
-  })
-}
+export const createGiftBox = giftBoxLoader.load.bind(giftBoxLoader)
