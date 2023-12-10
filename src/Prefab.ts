13396bdeb3315837f06bcfba1d95daff1cedb18e
Lajos Meszaros <m_lajos@hotmail.com>
2023-12-10 15:06:43 +0100

    feat(Prefab): introduce flipPolygonAxis* properties

--- src/Prefab.ts
+++ src/Prefab.ts
@@ -18,7 +18,7 @@ export type PrefabConstructorProps = {
   /**
    * default value is 1
    */
-  internalScale?: number
+  internalScale?: number | Vector3
   /**
    * default value is 0
    */
@@ -31,6 +31,19 @@ export type PrefabConstructorProps = {
    * default value is false
    */
   flipUVHorizontally?: boolean
+
+  /**
+   * default value is false
+   */
+  flipPolygonAxisX?: boolean
+  /**
+   * default value is false
+   */
+  flipPolygonAxisY?: boolean
+  /**
+   * default value is false
+   */
+  flipPolygonAxisZ?: boolean
   materialFlags?: (texture: Texture, defaultFlags: ArxPolygonFlags) => ArxPolygonFlags
 }
 
@@ -44,10 +57,9 @@ export class Prefab {
   filenameWithoutExtension: string
   objFolder: string
   textureFolder: string
-  internalScale: number
+  internalScale: Vector3
+  internalScaleUV: Vector2
   yAxisAdjustment: number
-  flipUVVertically: boolean
-  flipUVHorizontally: boolean
   materialFlags: ((texture: Texture, defaultFlags: ArxPolygonFlags) => ArxPolygonFlags) | undefined
 
   constructor({
@@ -58,27 +70,37 @@ export class Prefab {
     yAxisAdjustment = 0,
     flipUVVertically = false,
     flipUVHorizontally = false,
+    flipPolygonAxisX = false,
+    flipPolygonAxisY = false,
+    flipPolygonAxisZ = false,
     materialFlags,
   }: PrefabConstructorProps) {
     this.filenameWithoutExtension = filenameWithoutExtension
     this.objFolder = objFolder
     this.textureFolder = textureFolder
-    this.internalScale = internalScale
+
+    if (typeof internalScale === 'number') {
+      this.internalScale = new Vector3(internalScale, internalScale, internalScale)
+    } else {
+      this.internalScale = internalScale.clone()
+    }
+    this.internalScale.multiply(
+      new Vector3(flipPolygonAxisX ? -1 : 1, flipPolygonAxisY ? -1 : 1, flipPolygonAxisZ ? -1 : 1),
+    )
+
+    this.internalScaleUV = new Vector2(flipUVHorizontally ? -1 : 1, flipUVVertically ? -1 : 1)
+
     this.yAxisAdjustment = yAxisAdjustment
-    this.flipUVVertically = flipUVVertically
-    this.flipUVHorizontally = flipUVHorizontally
     this.materialFlags = materialFlags
   }
 
   async load({ position = new Vector3(0, 0, 0), orientation, scale: rawScale = 1 }: PrefabLoadProps = {}) {
-    const scale = this.internalScale * rawScale
-
-    const scaleUV = new Vector2(this.flipUVHorizontally ? -1 : 1, this.flipUVVertically ? -1 : 1)
+    const scale = this.internalScale.multiplyScalar(rawScale)
 
     return loadOBJ(path.join(this.objFolder, this.filenameWithoutExtension), {
-      position: position.clone().add(new Vector3(0, this.yAxisAdjustment * scale, 0)),
+      position: position.clone().add(new Vector3(0 * scale.x, this.yAxisAdjustment * scale.y, 0 * scale.z)),
       scale,
-      scaleUV,
+      scaleUV: this.internalScaleUV,
       orientation,
       materialFlags: (texture, flags) => {
         if (!texture.isInternalAsset) {
