3212597dbbb67e42a41bf87d762e3c3f0819827a
Lajos Meszaros <m_lajos@hotmail.com>
2023-12-10 01:48:41 +0100

    refactor(prefabs/hat): utilize Prefab class to shrink down code size

--- src/prefabs/hat.ts
+++ src/prefabs/hat.ts
@@ -1,28 +1,9 @@
-import { Rotation, Vector3 } from 'arx-level-generator'
-import { loadOBJ } from 'arx-level-generator/tools/mesh'
+import { Prefab } from '@/Prefab.js'
 
-type createHatProps = {
-  position?: Vector3
-  orientation?: Rotation
-  scale?: number
-}
+const hatLoader = new Prefab({
+  filenameWithoutExtension: 'Hat',
+  yAxisAdjustment: -130,
+  internalScale: 0.11,
+})
 
-export const createHat = async ({
-  position = new Vector3(0, 0, 0),
-  orientation,
-  scale: rawScale = 1,
-}: createHatProps = {}) => {
-  const scale = 0.1 * rawScale
-
-  return loadOBJ('./3d models + textures/Hat', {
-    position: position.clone().add(new Vector3(0, -130 * scale, 0)),
-    scale,
-    orientation,
-    materialFlags: (texture, flags) => {
-      if (!texture.isInternalAsset) {
-        texture.sourcePath = './3d models + textures/Texture/'
-      }
-      return flags
-    },
-  })
-}
+export const createHat = hatLoader.load.bind(hatLoader)
