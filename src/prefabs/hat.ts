c5307387f226aca7815edb547103fe2c14a85215
Lajos Meszaros <m_lajos@hotmail.com>
2023-12-10 14:10:42 +0100

    fix(prefabs/hat): shrink hat even further

--- src/prefabs/hat.ts
+++ src/prefabs/hat.ts
@@ -3,7 +3,7 @@ import { Prefab } from '@/Prefab.js'
 const prefab = new Prefab({
   filenameWithoutExtension: 'Hat',
   yAxisAdjustment: -130,
-  internalScale: 0.11,
+  internalScale: 0.09,
 })
 
 export const createHat = prefab.load.bind(prefab)
